2471e6748b73bd0d2763a3adca4903f01fe2e987
Shivam Patel <98849986+svp1996@users.noreply.github.com>
2023-02-16 00:50:15 -0600

    Create BennCollegeFootball.sql
    In this dataset we explore using the CASE statement in various scenarios:
    Adding multiple conditions to a CASE statement,
    Using CASE with aggregate functions, and
    Using CASE inside of aggregate functions
    This data was collected from ESPN in 2014

---
+++ BennCollegeFootball.sql
@@ -0,0 +1,161 @@
+SELECT * FROM benn.college_football_players
+
+/* Case statement is SQLs version of if/then logic */
+
+SELECT player_name,
+       year,
+       CASE WHEN year = 'SR' THEN 'yes'
+            ELSE NULL END AS is_a_senior
+  FROM benn.college_football_players
+  
+  /* To not have null values in the is_a_senior column, we use NO with ELSE statement */
+  
+  SELECT player_name,
+       year,
+       CASE WHEN year = 'SR' THEN 'yes'
+            ELSE 'no' END AS is_a_senior
+  FROM benn.college_football_players
+  
+/* Checking to see if has 'yes' flagged when a player is from California, 
+sorting the results with those players first */
+
+SELECT  player_name,
+        state,
+        CASE WHEN state = 'CA' THEN 'YES'
+        ELSE NULL END AS from_california
+    FROM benn.college_football_players
+    ORDER BY 3 
+    
+
+/* Defining number of outcomes in a CASE statement with the use of WHEN/THEN statements */
+
+SELECT player_name,
+       weight,
+       CASE WHEN weight > 250 THEN 'over 250'
+            WHEN weight > 200 THEN '201-250'
+            WHEN weight > 175 THEN '176-200'
+            ELSE '175 or under' END AS weight_group
+FROM benn.college_football_players
+
+/* Better way to write the above query to prevent overlap */
+SELECT player_name,
+      weight,
+      CASE WHEN weight > 250 THEN 'over 250'
+          WHEN weight > 200 AND weight <= 250 THEN '201-250'
+          WHEN weight > 175 AND weight <= 200 THEN '176-200'
+          ELSE '175 or under' END AS weight_group
+FROM benn.college_football_players
+
+/* Query classifies player names based on four categories of height */
+
+SELECT player_name,
+      Height,
+      CASE WHEN height > 74 THEN 'over 74'
+           WHEN height > 72 AND height <= 74 THEN '73-74'
+           WHEN height > 70 AND height <= 72 THEN '71-72'
+           ELSE 'under 70' END AS height_group
+FROM benn.college_football_players
+
+SELECT player_name,
+       CASE WHEN year = 'FR' AND position = 'WR' THEN 'frosh_wr'
+            ELSE NULL END AS sample_case_statement
+  FROM benn.college_football_players
+
+/* Query that adds additional column that displays players name if that player is a junior or senior */
+
+SELECT *,
+       CASE WHEN year IN ('JR', 'SR') THEN player_name ELSE NULL END AS upperclass_player_name
+  FROM benn.college_football_players
+  
+/* Using CASE with aggregate function */
+  
+SELECT CASE WHEN year = 'FR' THEN 'FR'
+            ELSE 'Not FR' END AS year_group,
+            COUNT(1) AS count
+  FROM benn.college_football_players
+ GROUP BY CASE WHEN year = 'FR' THEN 'FR'
+               ELSE 'Not FR' END
+               
+/* Count other conditions by using WHEN */               
+
+SELECT CASE WHEN year = 'FR' THEN 'FR'
+            WHEN year = 'SO' THEN 'SO'
+            WHEN year = 'JR' THEN 'JR'
+            WHEN year = 'SR' THEN 'SR'
+            ELSE 'No Year Data' END AS year_group,
+            COUNT(year) AS count
+  FROM benn.college_football_players
+ GROUP BY 1
+ 
+ 
+ SELECT CASE WHEN year = 'FR' THEN 'FR'
+            WHEN year = 'SO' THEN 'SO'
+            WHEN year = 'JR' THEN 'JR'
+            WHEN year = 'SR' THEN 'SR'
+            ELSE 'No Year Data' END AS year_group,
+            COUNT(1) AS count
+  FROM benn.college_football_players
+ GROUP BY CASE WHEN year = 'FR' THEN 'FR'
+               WHEN year = 'SO' THEN 'SO'
+               WHEN year = 'JR' THEN 'JR'
+               WHEN year = 'SR' THEN 'SR'
+               ELSE 'No Year Data' END
+               
+/* Write a query that counts the number of 300+lb palyers for regions west coast CA,OR,WA, Texas and other */
+
+SELECT CASE WHEN state IN ('CA', 'OR', 'WA') THEN 'West Coast'
+            WHEN state = 'TX' THEN 'Texas'
+            ELSE 'Other' END AS arbitrary_regional_designation,
+            COUNT(1) AS players
+        FROM benn.college_football_players
+        WHERE weight >= 300
+        GROUP BY 1
+        
+/* Write a query that calculates the combined weight of all underclass players (FR/SO)
+in CA and the combined weight of all upperclass players (JR/SR) in CA */
+
+SELECT CASE WHEN year IN ('FR', 'SO') THEN 'underclass'
+            WHEN year IN ('JR', 'SR') THEN 'uppperclass'
+            ELSE NULL END AS class_group,
+        SUM(weight) AS combined_player_weight
+    FROM benn.college_football_players
+    WHERE state = 'CA'
+    GROUP BY 1
+    
+/* Showing data horizontally, like a pivot table in Excel would */
+SELECT CASE WHEN year = 'FR' THEN 'FR'
+            WHEN year = 'SO' THEN 'SO'
+            WHEN year = 'JR' THEN 'JR'
+            WHEN year = 'SR' THEN 'SR'
+            ELSE 'No Year Data' END AS year_group,
+            COUNT(1) AS count
+  FROM benn.college_football_players
+ GROUP BY 1
+ 
+ SELECT COUNT(CASE WHEN year = 'FR' THEN 1 ELSE NULL END) AS fr_count,
+       COUNT(CASE WHEN year = 'SO' THEN 1 ELSE NULL END) AS so_count,
+       COUNT(CASE WHEN year = 'JR' THEN 1 ELSE NULL END) AS jr_count,
+       COUNT(CASE WHEN year = 'SR' THEN 1 ELSE NULL END) AS sr_count
+  FROM benn.college_football_players
+  
+/* Query to display the number of players in each state, with standings in separate columns,
+and another column with total number of players. Order results by states in desc */
+SELECT state,
+       COUNT (CASE WHEN year = 'FR' THEN 1 ELSE NULL END) AS fr_count,
+       COUNT (CASE WHEN year = 'SO' THEN 1 ELSE NULL END) AS so_count,
+       COUNT (CASE WHEN year = 'JR' THEN 1 ELSE NULL END) AS jr_count,
+       COUNT (CASE WHEN year = 'SR' THEN 1 ELSE NULL END) AS sr_count,
+       COUNT (1) AS total_players
+    FROM benn.college_football_players
+    GROUP BY state
+    ORDER BY total_players DESC
+
+/* Query explores the number of players at schools with names that start with A-M and # of 
+schools that start with N-Z */
+SELECT CASE WHEN school_name < 'n' THEN 'A-M'
+            WHEN school_name >= 'n' THEN 'N-Z'
+            ELSE NULL END AS school_name_group,
+            COUNT(1) AS players
+          FROM benn.college_football_players
+          GROUP BY 1
+          
